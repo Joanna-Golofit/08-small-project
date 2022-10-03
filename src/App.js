ac4f2f2cb23a4b454651194cebb496907e82ea87
Joanna Gołofit <jgolofit@future-processing.com>
2022-10-03 15:43:57 +0200

    checking previous state before changing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
 function App() {
-  const [usersList, setUsersList] = useState([{ name: "Asia", age: 37 }]);
+  const [usersList, setUsersList] = useState([{ name: "Asia", age: 37, id: "1xy"}]);
   // const [users, setUsers] = useState([]);
 
   const addToUsersListHandler = (username, age) => {
-    setUsersList([{ name: username, age }, ...usersList])
+    setUsersList(usersList => [{ name: username, age, id: Date.now() }, ...usersList])
   }
   return (
     <>
