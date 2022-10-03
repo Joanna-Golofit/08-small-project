43361ce95e73515e0952d4821a2c715bf762bcaf
Joanna Gołofit <jgolofit@future-processing.com>
2022-10-03 14:27:13 +0200

    95. Adding a Users List Component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import Button from '../UI/Button'
 import Card from '../UI/Card';
 import styles from './AddUser.module.css'
+import UsersList from './UsersList';
 
 const AddUser = () => {
   const [username, setUsername] = useState("");
   const [age, setAge] = useState("");
+  const [users, setUsers] = useState([{name: "Asia", age: 37}]);
 
   const addUserHandler = (e) => {
     e.preventDefault();
@@ -18,6 +20,7 @@ const AddUser = () => {
       return;
     }
     console.log(username, age);
+    addToUsersHandler();
     clearInputs();
   };
 
@@ -34,24 +37,35 @@ const AddUser = () => {
     }
     setAge(e.target.value);
   }
+
   const clearInputs = () => {
     setUsername("");
     setAge("");
+  };
+
+  const addToUsersHandler = () => {
+    setUsers([{ name: username, age }, ...users])
   }
 
 
   return (
-    <Card className={styles.input}>
-      <form
-        onSubmit={addUserHandler}
-      >
-        <label htmlFor='username'>Username</label>
-        <input id="username" type="text" value={username} onChange={enteredUsernameHandler} ></input>
-        <label htmlFor='age'>Age (years)</label>
-        <input id="age" type="number" value={age} onChange={enteredAgeHandler} ></input>
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <>
+      <Card className={styles.input}>
+        <form
+          onSubmit={addUserHandler}
+        >
+          <label htmlFor='username'>Username</label>
+          <input id="username" type="text" value={username} onChange={enteredUsernameHandler} ></input>
+          <label htmlFor='age'>Age (years)</label>
+          <input id="age" type="number" value={age} onChange={enteredAgeHandler} ></input>
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+      <Card className={styles.input}>
+        <UsersList users={users} />
+      </Card>
+    </>
+
   )
 }
 
---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from '../UI/Card'
+
+const UsersList = (props) => {
+  return (
+    <Card>
+      <ul>{props.users.map(user => <li key={user.id}>{user.name} ({user.age} years old)</li>)}</ul>
+    </Card>
+  )
+}
+
+export default UsersList
