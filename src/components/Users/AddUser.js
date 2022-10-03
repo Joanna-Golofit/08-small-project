815f402f3e3e309f64058eda9da0d010f7e48a67
Joanna Gołofit <jgolofit@future-processing.com>
2022-10-03 13:11:44 +0200

    first commit

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import Button from '../UI/Button'
+import Card from '../UI/Card';
+import styles from './AddUser.module.css'
+
+const AddUser = () => {
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
+
+  const addUserHandler = (e) => {
+    e.preventDefault();
+    if (username.trim().length === 0 || age.trim().length === 0) {
+      console.log("Empty input(s) not allowed!");
+      return;
+    }
+    if (Number(age) < 1) {
+      console.log("Age has to be greater than 0!");
+      return;
+    }
+    console.log(username, age);
+    clearInputs();
+  };
+
+  const enteredUsernameHandler = (e) => {
+    if (e.target.value.trim().length === 0) {
+      return;
+    }
+    setUsername(e.target.value);
+  }
+
+  const enteredAgeHandler = (e) => {
+    if (e.target.value.trim().length === 0) {
+      return;
+    }
+    setAge(e.target.value);
+  }
+  const clearInputs = () => {
+    setUsername("");
+    setAge("");
+  }
+
+
+  return (
+    <Card className={styles.input}>
+      <form
+        onSubmit={addUserHandler}
+      >
+        <label htmlFor='username'>Username</label>
+        <input id="username" type="text" value={username} onChange={enteredUsernameHandler} ></input>
+        <label htmlFor='age'>Age (years)</label>
+        <input id="age" type="number" value={age} onChange={enteredAgeHandler} ></input>
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  )
+}
+
+export default AddUser
