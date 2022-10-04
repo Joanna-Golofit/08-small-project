0a17e10d239843c15406f099605d288259f5224f
Joanna Gołofit <jgolofit@future-processing.com>
2022-10-04 10:34:04 +0200

    adding backdrop

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -7,17 +7,19 @@ import styles from './ErrorModal.module.css'
 const ErrorModal = (props) => {
   console.log(props);
   return (
-    <Card className={styles.modal}>
-      <header className={styles.header}>
-        <h2>ErrorModal {props.modalMessage.title}</h2>
-      </header>
-      <div className={styles.content}>
-        <p>{props.modalMessage.message}</p>
-      </div>
-      <footer className={styles.actions}>
-        <Button onClick={props.showModalHandler}>OK</Button>
-      </footer>
-    </Card>
+    <div className={styles.backdrop}>
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>ErrorModal {props.modalMessage.title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{props.modalMessage.message}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button onClick={props.showModalHandler}>OK</Button>
+        </footer>
+      </Card>
+    </div>
   )
 }
 
