905316d420866cd099403ee292b777f3f2386e8e
Joanna Gołofit <jgolofit@future-processing.com>
2022-10-12 11:23:46 +0200

    106. Working with "ref"s" - useRef instead of useState in inputs = less code

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Button from '../UI/Button'
 import Card from '../UI/Card';
 import ErrorModal from '../UI/ErrorModal';
 import styles from './AddUser.module.css'
 
 const AddUser = (props) => {
-  const [username, setUsername] = useState("");
-  const [age, setAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  // const [username, setUsername] = useState("");
+  // const [age, setAge] = useState("");
   const modalMessages = [
     { title: "Invalid input", message: "Empty input(s) not allowed!" },
     { title: "Invalid age", message: "Age has to be greater than 0!" },
@@ -15,33 +18,38 @@ const AddUser = (props) => {
 
   const addUserHandler = (e) => {
     e.preventDefault();
-    if (username.trim().length === 0 || age.trim().length === 0) {
+    console.log("nameInputRef", nameInputRef.current.value);
+    const enteredName =  nameInputRef.current.value;
+    const enteredUserAge =  ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       console.log("Empty input(s) not allowed!");
       setModalMessage(modalMessages[0])
       return;
     }
-    if (Number(age) < 1) {
+    if (Number(enteredUserAge) < 1) {
       console.log("Age has to be greater than 0!");
       setModalMessage(modalMessages[1]);
       return;
     }
-    console.log(username, age);
-    props.addToUsersListHandler(username, age);
-    clearInputs();
+    console.log(enteredName, enteredUserAge);
+    props.addToUsersListHandler(enteredName, enteredUserAge);
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
+    // clearInputs();
   };
 
-  const enteredUsernameHandler = (e) => {
-    setUsername(e.target.value);
-  }
+  // const enteredUsernameHandler = (e) => {
+  //   setUsername(e.target.value);
+  // }
 
-  const enteredAgeHandler = (e) => {
-    setAge(e.target.value);
-  }
+  // const enteredAgeHandler = (e) => {
+  //   setAge(e.target.value);
+  // }
 
-  const clearInputs = () => {
-    setUsername("");
-    setAge("");
-  };
+  // const clearInputs = () => {
+  //   setUsername("");
+  //   setAge("");
+  // };
 
   const clearModalMessage = (e) => {
     setModalMessage();
@@ -52,9 +60,20 @@ const AddUser = (props) => {
       <Card className={styles.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor='username'>Username</label>
-          <input id="username" type="text" value={username} onChange={enteredUsernameHandler} ></input>
+          <input
+            ref={nameInputRef}
+            id="username" type="text"
+            // value={username}
+            // onChange={enteredUsernameHandler}
+          />
           <label htmlFor='age'>Age (years)</label>
-          <input id="age" type="number" value={age} onChange={enteredAgeHandler} ></input>
+          <input
+            ref={ageInputRef}
+            id="age"
+            type="number"
+            // value={age}
+            // onChange={enteredAgeHandler}
+          />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
